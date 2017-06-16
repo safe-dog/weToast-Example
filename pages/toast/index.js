f3288ef91df43303590657a871e5978803a2650e
safe-dog <admin@safedog.cc>
2017-06-16 18:05:58 +0800

    简化代码

--- pages/toast/index.js
+++ pages/toast/index.js
@@ -8,8 +8,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    defaultTitle: '提示标题',
-    defaultContent: '这是一条提示信息内容！'
+    title: '提示标题',
+    content: '这是一条提示信息内容！',
   },
 
   /**
@@ -17,23 +17,8 @@ Page({
    */
   onLoad: function (options) {
     toast = new weToast(this);
-    toast.setDelay(5000);
+    toast.setDelay(2000);
     toast.success('欢迎使用weToast！') ;
-  },
-
-  showSuccess: () => {
-    toast.success('不错哦')
-  },
-
-  showWarning: () => {
-    toast.warning('这是一条警告消息⚠️\n哈哈是否打算\n哈哈哈\n东方拉基舍夫撒地方\n分撒娇阿斯顿发生的', '警告拉到饭卡叫拉风的时代里发酵的饭卡附近的 \n哈哈哈');
-  },
-
-  showInfo: () => {
-    toast.info('提示消息');
-  },
-  showError: () => {
-    toast.error('错误啦')
   },
 
   /**
@@ -63,25 +48,22 @@ Page({
   onUnload: function () {
   
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
   
+  inputTitleHandler: function (e) {
+    const { value } = e.detail;
+    this.setData({
+      title: value
+    });
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
+  inputContentHandler: function (e) {
+    const { value } = e.detail;
+    this.setData({
+      content: value
+    });
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  toastHandler: function (e) {
+    const { icon } = e.currentTarget.dataset;
+    const { title, content } = this.data;
+    toast[icon](content, title);
   }
 })
